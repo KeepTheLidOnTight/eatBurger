aa203851d22164b85099d4c6d63fec97adefd4f5
Ryan Freese <65748652+KeepTheLidOnTight@users.noreply.github.com>
2020-10-30 18:11:06 -0500

    Add files via upload

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("The Boss Burger",true);
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("The Juicy Lucy",true);
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("The Quadruple Bypass Burger",false);
